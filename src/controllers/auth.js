23dbbe5004ae88527032926c43a407951b0fa4ba
Lucas Macias <chueko@gmail.com>
2023-05-20 16:25:32 -0300

    feat: add forgot and reset password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,35 +3,31 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import _ from "lodash";
 import Config from "../config/config.js";
 
 export const signUp = async (req, res) => {
     const { name, email, password } = req.body;
 
     try {
-        //check if user already exists
         const userExists = await User.findOne({ email });
         if (userExists)
             return res.status(400).json({ message: "User already exists" });
 
-        //hash password
         const hashedPassword = await bcrypt.hash(password, 12);
 
-        //create new user
         const newUser = await User.create({
             name,
             email,
             password: hashedPassword,
         });
 
-        //sign token
         const token = jwt.sign(
             { email: newUser.email, id: newUser._id },
             Config.mongoDB.secret,
             { expiresIn: "1h" }
         );
 
-        //send response
         res.status(201).json({ result: newUser, token });
     } catch (error) {
         res.status(500).json({ message: "Something went wrong", error });
@@ -42,25 +38,119 @@ export const signIn = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        //check if user exists
         const user = await User.findOne({ email });
         if (!user)
             return res.status(404).json({ message: "User doesn't exist" });
 
-        //check if password is correct
         const isPasswordCorrect = await bcrypt.compare(password, user.password);
         if (!isPasswordCorrect)
             return res.status(400).json({ message: "Invalid credentials" });
 
-        //sign token
         const token = jwt.sign(
             { email: user.email, id: user._id },
             Config.mongoDB.secret,
             { expiresIn: "1d" }
         );
 
-        //send response
         res.status(200).json({ result: user, token });
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" }, error);
+    }
+};
+
+// forgot password
+
+export const forgotPassword = async (req, res) => {
+    try {
+        const { email } = req.body;
+        User.findOne({ email }, (err, user) => {
+            if (err || !user)
+                return res.status(401).json({
+                    error: "User with this email does not exist",
+                });
+
+            const token = jwt.sign({ _id: user._id }, Config.mongoDB.secret, {
+                expiresIn: "30m",
+            });
+
+            const data = {
+                from: "",
+                to: email,
+                subject: "Password Reset Link",
+                html: `
+                    <h2>Please click on given link to reset your password</h2>
+                    <p>${Config.clientURL}/resetpassword/${token}</p>
+                `,
+            };
+            return user.updateOne({ resetLink: token }, (err, success) => {
+                if (err) {
+                    return res.status(400).json({
+                        error: "Reset password link error",
+                    });
+                } else {
+                    // TODO send email with nodemailer
+                    transporter.sendMail(data, (err, info) => {
+                        if (err) {
+                            return res.status(400).json({
+                                error: "Something went wrong",
+                            });
+                        } else {
+                            return res.status(200).json({
+                                message: `Email has been sent to ${email}. Follow the instruction to activate your account`,
+                            });
+                        }
+                    });
+                }
+            });
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" }, error);
+    }
+};
+
+// reset password
+
+export const resetPassword = async (req, res) => {
+    try {
+        const { resetLink, newPass } = req.body;
+        if (resetLink) {
+            jwt.verify(
+                resetLink,
+                Config.mongoDB.secret,
+                function (error, decoded) {
+                    if (error) {
+                        return res.status(401).json({
+                            error: "Incorrect token or it is expired",
+                        });
+                    }
+                    User.findOne({ resetLink }, (err, user) => {
+                        if (err || !user) {
+                            return res.status(400).json({
+                                error: "User with this token does not exist",
+                            });
+                        }
+                        const obj = {
+                            password: newPass,
+                            resetLink: "",
+                        };
+                        user = _.extend(user, obj);
+                        user.save((err, result) => {
+                            if (err) {
+                                return res.status(400).json({
+                                    error: "Reset password error",
+                                });
+                            } else {
+                                return res.status(200).json({
+                                    message: "Your password has been changed",
+                                });
+                            }
+                        });
+                    });
+                }
+            );
+        } else {
+            return res.status(401).json({ error: "Authentication error" });
+        }
     } catch (error) {
         res.status(500).json({ message: "Something went wrong" }, error);
     }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,17 +1,29 @@
 import { Router } from "express";
-import { signUp, signIn } from "../controllers/auth.js";
+import {
+    signUp,
+    signIn,
+    resetPassword,
+    forgotPassword,
+} from "../controllers/auth.js";
 
 import { runValidation } from "../validators/index.js";
 import {
     userSignupValidator,
     userSigninValidator,
 } from "../validators/auth.js";
+
 const authRouter = Router();
 
 //Login
-authRouter.post("/login", userSigninValidator, signIn);
+authRouter.post("/login", userSigninValidator, runValidation, signIn);
 
 //Register
 authRouter.post("/register", userSignupValidator, runValidation, signUp);
+
+//Forgot Password
+authRouter.post("/forgot-password", forgotPassword); //TO TRY
+
+//Reset Password
+authRouter.post("/reset-password", resetPassword); //TO TRY
 
 export default authRouter;
